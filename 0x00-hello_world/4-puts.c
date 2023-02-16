435ee1c13d1a2c8fa0824ff9927c36187f3559fa
MarB-T <danmarbet@gmail.com>
2023-02-16 12:45:11 +0300

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	const char mystr[]='"Programming is like building a multilingual puzzle';
+	const char mystr[]="\"Programming is like building a multilingual puzzle";
  	 puts(mystr);
 	return 0;
 
