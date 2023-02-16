2fb89bd7552f72a29be0c5acb267ad604b153588
MarB-T <danmarbet@gmail.com>
2023-02-17 00:04:15 +0300

    Rectifying to complly with Betty convention

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,21 +2,22 @@
 
 /**
  * main - Entry point
- * Description: This function uses inline assembly to print a string to the console using the syscall function in x86_64 architecture.
+ * Description: Uses inline assembly to print a string to the console.
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-        char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        long l = 59;
-        long fd = 1;
-        long syscall = 1;
-        long ret = 0;
-        __asm__ ("syscall"
-                : "=a" (ret)
-                : "a" (syscall),
-                  "D" (fd),
-                  "S" (s),
-                  "d" (l));
-        return (1);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+
+	__asm__ ("syscall"
+		: "=a" (ret)
+		: "a" (syscall),
+		"D" (fd),
+		"S" (s),
+		"d" (l));
+		return (1);
 }
