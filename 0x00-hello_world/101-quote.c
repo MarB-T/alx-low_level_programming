cac063ff03e1b24a4ffd8100e8610d73b899b6dd
Marbet K Daniel <danmarbet@gmail.com>
2023-02-16 12:05:05 -0800

    Output to console without using prinf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * main - print a string without using puts or printf
  * Return: 1 (Success)
  */
-int main(void)
+int main()
 {
 	const char str[] = "(and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n";
 	fputs(str, stdout);
