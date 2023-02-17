d5f995304ad9c31012eceb3399c6d171476a509a
MarB-T <danmarbet@gmail.com>
2023-02-17 17:53:15 +0300

    Both upper and lowercase alphabet using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Diplay the alphabet in lower case using putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97;
+	int x = 65;
+
+	for (a = 97; a <= 122; a++)
+	{
+		char b = a;
+
+		putchar(b);
+	}
+	for (x = 65; x <= 90; a++)
+	{
+		char b = x;
+
+		putchar(b);
+	}
+
+	putchar('\n');
+	return (0);
+}
