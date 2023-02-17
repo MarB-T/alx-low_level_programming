58faf6d204b2c17b49b9e40cdf6fbb86e9d1be75
MarB-T <danmarbet@gmail.com>
2023-02-17 21:32:53 +0300

    Two none similar digit pairs in order

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print all two non-similar digit combinations in order.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+	int y = 1;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			if ((x == y) || (x > y))
+			{
+			}
+			else
+			{
+				putchar((char)x);
+				putchar((char)y);
+				if ((x == 8) && (y == 9))
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
