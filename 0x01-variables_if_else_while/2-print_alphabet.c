61e7bf91d378afb6609770569599a12865d23ec7
MarB-T <danmarbet@gmail.com>
2023-02-17 17:43:14 +0300

    Display alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Diplay the alphabet in lower case using putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	for (a = 97; a <= 122; a++)
+	{
+		char b = a;
+
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
