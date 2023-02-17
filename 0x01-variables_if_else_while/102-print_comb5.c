2418715a773c01f5c27dec4f55c1e42635d9d81b
MarB-T <danmarbet@gmail.com>
2023-02-17 22:55:29 +0300

    Yet another conbination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Print all two non-similar digit combinations in order.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int w = 1;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				for (w = 1; w <= 9; w++)
+				{
+					if (y > w)
+					{
+					}
+					else
+					{
+						putchar(x + '0');
+						putchar(y + '0');
+						putchar(' ');
+						putchar(z + '0');
+						putchar(w + '0');
+							/*
+							 * if ((x == 7) && (y == 8) && (z == 9))
+							 * {
+							 *	break;
+							 * }
+							*/
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
