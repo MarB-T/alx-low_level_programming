9af6f0db77bc0d369df018a0287988b7a530d3ec
MarB-T <danmarbet@gmail.com>
2023-02-17 19:43:03 +0300

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Using putchar to dispaly single digit numbers 0-9
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		char b = a;
+
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
