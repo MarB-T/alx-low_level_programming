1fb891c5b9749eb5da9490886ed4387e382c1f9e
MarB-T <danmarbet@gmail.com>
2023-02-17 16:57:00 +0300

    Correcting directories

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *  more headers goes there 
+ *  main - Tell if number is positive or neagtive
+ *  return 0 (Success)
+ *  betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -13,15 +15,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d: is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d: is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d: is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
