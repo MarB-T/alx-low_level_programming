9340a87b335098ed532dbe01ecefd4c38dfe8896
MarB-T <danmarbet@gmail.com>
2023-02-17 19:48:58 +0300

    Numbers 0-9 using putchar separated by ","

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Using putchar to dispaly single digit numbers 0-9
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
