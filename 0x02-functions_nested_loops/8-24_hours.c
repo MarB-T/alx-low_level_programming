a95af69a2e5eaa6de18a60baf7e355c452e8456d
MarB-T <danmarbet@gmail.com>
2023-02-21 17:14:32 +0300

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
 
 void jack_bauer(void)
 {
-	int h = 1;
+	int h;
 	int m;
 
-	for (h = 1; h < 24; h++)
+	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			putchar((h/10) + '0');
-			putchar((h%10) + '0');
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
 			putchar(':');
-			putchar((m/10) + '0');
-			putchar((m%10) + '0');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
 			putchar('\n');
 		}
 	}
