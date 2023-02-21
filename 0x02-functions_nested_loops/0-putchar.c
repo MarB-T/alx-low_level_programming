96e44eb14fa196c2fd5f04673b695cacafc7708f
MarB-T <danmarbet@gmail.com>
2023-02-21 10:59:53 +0300

    Use print function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints string to console
+ * Return: 0 - Success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+\tFunctions and nested loops
