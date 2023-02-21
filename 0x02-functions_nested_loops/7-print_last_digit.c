85e923ee7673db0d8bc1cee7378281be830d8a37
MarB-T <danmarbet@gmail.com>
2023-02-21 20:07:41 +0300

    To 98

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,18 @@
 /**
  * print_last_digit - Prints last digit of a number
  * @n: number to get last digit
- * Return: Last_digit of n
+ * Return: Last_digit(a) of n
  */
 
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n = -n;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int a;
+
+	a = n % 10;
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
 }
