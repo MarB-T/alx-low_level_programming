23784f629efd93378eda0249b3ebc9f2f908806f
MarB-T <danmarbet@gmail.com>
2023-02-21 13:56:23 +0300

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -15,15 +15,3 @@ int _islower(int c)
 		return (0);
 	}
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
