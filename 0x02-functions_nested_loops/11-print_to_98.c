e3b93c7039750d3e085d302a79ae14728f6ff4a7
MarB-T <danmarbet@gmail.com>
2023-02-21 19:33:55 +0300

    To 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	int i;
 	if (n <= 98)
 	{
-		for (i == n; i == 98; i++)
+		for (i = n; i == 98; i++)
 		{
 			if ((i >= -9) && ( i <= 9))
 			{
@@ -29,7 +29,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i == n; i == 98; i--)
+		for (i = n; i == 98; i--)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
