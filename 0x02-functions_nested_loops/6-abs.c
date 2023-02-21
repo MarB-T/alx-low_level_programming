c4068ab85241ebfcd0ed21570c99cf997f74c58a
MarB-T <danmarbet@gmail.com>
2023-02-21 16:27:07 +0300

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _abs - Finds the absolute value of an integer
+ * @i: given number
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+}
