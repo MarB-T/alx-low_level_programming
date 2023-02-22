1d771e6fcd8a6ae97c09dab2627feabb6983227c
MarB-T <danmarbet@gmail.com>
2023-02-22 21:47:47 +0300

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,15 +8,15 @@
  * Return: largest number
  */
 
-int largest_number(unsigned int a, unsigned int b, unsigned int c)
+int largest_number(int a, int b, int c)
 {
-	unsigned int largest;
+	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
