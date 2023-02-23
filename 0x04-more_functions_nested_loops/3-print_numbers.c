fde93ba9e0ed528757005aa99106c23608c95470
MarB-T <danmarbet@gmail.com>
2023-02-23 22:04:23 +0300

    Numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h"
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ * Return: 0 
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar('i');
+	}
+	putchar('\n');
+}
