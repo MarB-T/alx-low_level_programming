8c28f6bb38896c930225ffc7e83472fe03dcbf21
MarB-T <danmarbet@gmail.com>
2023-02-24 04:44:48 +0300

    Prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: calls prime_factors
+ * Return: 0
+ */
+
+int main(void)
+{
+	prime_factors(612852475143);
+	return (0);
+}
+
+/**
+ * prime_factors - function definition
+ * Description: finds the largest prime factor
+ * @test_num: the number to test
+ * Return: void
+ */
+
+void prime_factors(long test_num)
+{
+	long divisor = 2;
+
+	while (divisor != test_num)
+	{
+		if (test_num % divisor == 0)
+		{
+			test_num /= divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+	printf("%ld\n", test_num);
+}
