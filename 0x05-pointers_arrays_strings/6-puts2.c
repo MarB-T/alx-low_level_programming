93d9b7fcba7e75e563077bf7e0ee51f8e848bafc
MarB-T <danmarbet@gmail.com>
2023-02-27 14:58:24 +0300

    Puts alternately

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: the string in question
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int m;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str -= len;
+	for (m = 0; m < len; m++)
+	{
+		if (m % 2 == 0)
+		{
+			_putchar(*(str + m));
+		}
+		else
+		{
+			contineu;
+		}
+	}
+	_putchar('\n');
+}
