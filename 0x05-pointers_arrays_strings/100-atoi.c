4e986a725f3cd7d7f027f415bc35cf0c240b06bc
MarB-T <danmarbet@gmail.com>
2023-02-27 17:46:44 +0300

    String to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,23 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	if (s[0] == '-')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i] != '\0'; i++)
-	{
-		result = result * 10 + s[i] - '0';
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] == '+')
+		{
+			sign *= 1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + s[i] - '0';
+		}
+		else
+		{
+		}
 	}
 	return (result * sign);
 }
