c8283f19b8f4522c95768901e518a1cc318a2650
MarB-T <danmarbet@gmail.com>
2023-02-27 15:32:36 +0300

    Print given number of array elements correction2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", *a);
+		printf("%d", *a);
+		if (i != (n - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 		a++;
 	}
 }
