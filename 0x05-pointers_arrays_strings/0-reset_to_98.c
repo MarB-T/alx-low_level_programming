5552d9c8c0c66d170af2b38533ab7d7829c369b9
MarB-T <danmarbet@gmail.com>
2023-02-27 10:21:09 +0300

    Pointers Arrays and Stringd

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update value stored in *n to 98
+ * @n: Pointer 
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+	Pointers Arrays and Strings
+0-reset_to_98 - update value poited by given integer pointer
+1-swap - Swaps values of two integers
+
