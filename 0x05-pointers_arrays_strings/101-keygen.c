2c14b24bd95e6fa634a053a6fc0f0dd0e7ffdaeb
MarB-T <danmarbet@gmail.com>
2023-02-27 18:41:43 +0300

    Random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,62 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * randomPassword - generates random password
+ * @n: Length of password
+ */
+void randomPassword(int n)
+{
+	int i = 0;
+	int randomizer = 0;
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[n];
+
+	srand((unsigned int)(time(NULL)));
+
+	randomizer = rand() % 4;
+
+	// Iterate over the range [0, N]
+	for (i = 0; i < n; i++) {
+
+		if (randomizer == 1) {
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2) {
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3) {
+			password[i] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else {
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+// Driver Code
+int main()
+{
+	// Length of the password to
+	// be generated
+	int n = 10;
+
+	// Function Call
+	randomPassword(n);
+
+	return 0;
+}
+
