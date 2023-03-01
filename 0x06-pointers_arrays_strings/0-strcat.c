b639dbf5559195e0d249fb95f5adc5bd9a826570
MarB-T <danmarbet@gmail.com>
2023-03-01 22:34:01 +0300

    Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,34 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1 = _strlen(dest);
-	char *ptr = dest + len1;
+	int index = 0;
+	int dest_len = 0;
 
-	while (*src != '\0')
+	while (dest[index++])
 	{
-		*ptr = *src;
-		src++;
-		ptr++;
+		dest_len++;
 	}
-	src -= _strlen(src);
-	*ptr = '\n';
+	for (index = 0; src[index]; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
 	return (dest);
-}
-
-/**
- * _strlen - determine length of a string
- * @s: String whose length to find
- * Return: string lenght
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
 }
