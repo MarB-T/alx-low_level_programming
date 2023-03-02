3cda522c932e01fbaabcb517d5276e3e5f1ed736
MarB-T <danmarbet@gmail.com>
2023-03-02 22:34:01 +0300

    Corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,20 @@
 #include "main.h"
-
 /**
- * _strncpy - Function that copies a string and append NULL if less characters
- * @dest: Pointer to first char of destination
- * @src: Pointer to first char of source
- * @n: Number of characters to be copied.
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index, len2;
+	int i;
 
-	while (src[index++])
-	{
-		len2++;
-	}
-	for (index = 0; src[index] && index < n; index++)
-	{
-		dest[index] = src[index];
-	}
-	for (index = len2; index < n; index++)
-	{
-		dest[index] = '\0';
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
