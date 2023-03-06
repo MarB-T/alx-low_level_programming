6aae01bb446dded68b3eaf340344521c3f5cd570
MarB-T <danmarbet@gmail.com>
2023-03-06 23:07:37 +0300

    Needle in haystack correction

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -26,7 +26,7 @@ char *_strstr(char *haystack, char *needle)
 		haystack++;
 	}
 	if (*needle == '\0')
-		return (NULL);
+		return (haystack);
 	while (*(needle + len2) != '\0')
 	{
 		if (*(needle + len2) != *(haystack + len2))
