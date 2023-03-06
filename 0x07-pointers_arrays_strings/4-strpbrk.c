55d1190bc6ce0a5f6d2ef67ecd72e6535e388860
MarB-T <danmarbet@gmail.com>
2023-03-06 17:39:13 +0300

    search string for any of a set of bytes correction

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strpbrk - serches for the first occurence of any of a set of bytes in a string.
+ * _strpbrk - find the first occurence of any of a set of bytes in a string.
  * @s: The string in question
  * @accept: char-set to check for
  * Return: pointer to the first occurence
@@ -11,7 +11,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
-	
+
 	while (*s != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
