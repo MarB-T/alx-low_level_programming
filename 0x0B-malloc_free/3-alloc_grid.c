84a94aa741f23ad0ac2829eaac62cad5eafac982
MarB-T <danmarbet@gmail.com>
2023-03-13 20:58:28 +0300

    malloc task 3 allocate memory to 2d array (correction)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,19 +29,17 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+			for (j = 0; j < i; j++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
 			return (NULL);
-	}
 
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
-	}
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
 	}
 	return (grid);
 }
