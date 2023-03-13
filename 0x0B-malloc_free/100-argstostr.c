f2f372f3952212c394f06c174d79007dab36fc6c
MarB-T <danmarbet@gmail.com>
2023-03-13 23:36:33 +0300

    malloc task 5 - concatenate arguments of a program correction

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ int _strlen(char *s)
 /**
  * argstostr - concatenates arguments of the program
  * @ac: argument count
- * @av: argument vector 
+ * @av: argument vector
  * Return: pointer or NULL
  */
 
@@ -40,7 +40,7 @@ char *argstostr(int ac, char **av)
 	{
 		n += _strlen(av[i]);
 	}
-	size = n + ac;
+	size = n + ac - 1;
 	str = (char *)malloc(sizeof(char) * size);
 	if (str == NULL)
 	{
