a8ad9d79a6a82c71c44bcde2a9fe3baf6952a589
MarB-T <danmarbet@gmail.com>
2023-03-13 16:09:27 +0300

    malloc task 2- concatenate strings correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,6 +37,14 @@ char *str_concat(char *s1, char *s2)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	n = len1 + len2 + 1;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	new = malloc(sizeof(char) * n);
 	if (new == NULL)
 		return (NULL);
