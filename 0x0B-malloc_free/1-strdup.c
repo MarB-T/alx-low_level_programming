6fad458ec56c421e9d1557125a5479c55215af61
MarB-T <danmarbet@gmail.com>
2023-03-13 14:45:44 +0300

    malloc and free task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string in a new location
+ * @str: the string in question
+ * Return: NULL or pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	int len = 0, i;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str -= len;
+	new = malloc((sizeof(char) * len) + 1);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		if (i == len)
+		{
+			new[i] = '\0';
+			return (new);
+		}
+		new[i] = str[i];
+	}
+	return (new);
+}
