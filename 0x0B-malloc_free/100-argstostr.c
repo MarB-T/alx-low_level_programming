7e21acad850f2207b901981ffc1dd2c53efb694c
MarB-T <danmarbet@gmail.com>
2023-03-13 23:28:39 +0300

    malloc task 5 - concatenate arguments of a program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - gets length of string
+ * @s: the string in question
+ * Return: lenght (int)
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * argstostr - concatenates arguments of the program
+ * @ac: argument count
+ * @av: argument vector 
+ * Return: pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, n = 0, size = 0, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		n += _strlen(av[i]);
+	}
+	size = n + ac;
+	str = (char *)malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	return (str);
+
+}
