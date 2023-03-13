aec4db68a3377c64ccac441f9a7223fc76ba11ee
MarB-T <danmarbet@gmail.com>
2023-03-13 18:52:50 +0300

    malloc task 3 - allocate memory to 2d array using double pointer

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * alloc_grid - allocate memory to 2D array
+ * @width: width of array
+ * @height: height of array
+ * Return: pointer or NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
