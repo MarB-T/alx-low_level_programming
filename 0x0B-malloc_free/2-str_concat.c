3fccb211263ba529e9cb7ff92ae04e9b2825aeec
MarB-T <danmarbet@gmail.com>
2023-03-13 15:36:34 +0300

    malloc task 2- concatenate strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strlen - determines the length of string
+ * @s: string in questio
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+		return (0);
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new string or NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1, len2, n;
+	char *new;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	n = len1 + len2 + 1;
+	new = malloc(sizeof(char) * n);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		new[i] = s1[i];
+	}
+	for(; i <= n; i++)
+	{
+		if (i == n)
+		{
+			new[i] = '\0';
+			return (new);
+		}
+		new[i] = s2[i - len1];
+	}
+	return (new);
+}
