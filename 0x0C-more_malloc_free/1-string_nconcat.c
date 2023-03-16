6c7788cdeac331d2b1fe03b1844f140279855089
MarB-T <danmarbet@gmail.com>
2023-03-16 13:54:42 +0300

    more malloc and free task 1, concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 /**
- * string_nconcat concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: size of s2 to concatenate
