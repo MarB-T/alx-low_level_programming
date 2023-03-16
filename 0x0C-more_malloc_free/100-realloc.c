556519581a4139a60a703a8dba9e6e5a6455817a
MarB-T <danmarbet@gmail.com>
2023-03-16 21:32:15 +0300

    more malloc & free task 4 implementing realloc using malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - realocates mempry using malloc and free
+ * @ptr: pointer to previously allocated mempry
+ * @old_size: size in bytes of old allocation
+ * @new_size: new size in bytes
+ * Return: pointer to new memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *new_ptr, *ptrr = ptr;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < (new_size || old_size); i++)
+	{
+		new_ptr[i] = ptrr[i];
+	}
+	free(ptr);
+	return (new_ptr);
+
+}
