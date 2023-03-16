dbdb3b82890d3a5734597a3adb8ad10a6cd2f7f2
MarB-T <danmarbet@gmail.com>
2023-03-16 13:50:00 +0300

    more malloc and free task 1, concatenate two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: size of s2 to concatenate
+ * Return: pointer or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len1, i = 0;
+	char *res;
+
+	res = malloc(sizeof(s1) + n + 1);
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+	len1 = strlen(s1);
+	for (i = 0; i < (len1 + n); i++)
+	{
+		if (i < len1)
+		{
+			res[i] = s1[i];
+		}
+		if (i >= len1)
+		{
+			res[i] = s2[(i - len1)];
+		}
+	}
+	res[i] = '\0';
+	return (res);
+}
