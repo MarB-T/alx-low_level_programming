2f875b9aa6a5524aec1520f5cc2ca829bb4c438a
MarB-T <danmarbet@gmail.com>
2023-03-16 19:11:55 +0300

    more malloc and free task 3, creates array of integers in order

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -28,31 +28,3 @@ int *array_range(int min, int max)
 	}
 	return (ptr);
 }
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
