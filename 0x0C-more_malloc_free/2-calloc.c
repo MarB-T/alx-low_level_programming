53d66d42df01b74c2d9347d556582dec84b1d2d5
MarB-T <danmarbet@gmail.com>
2023-03-16 17:23:50 +0300

    more malloc and free task 2 - calloc implementation using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - implementation of calloc
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
