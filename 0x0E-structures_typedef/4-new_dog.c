057773ad0e06a230a807ce35f5efdf7b88f4eef7
MarB-T <danmarbet@gmail.com>
2023-03-21 00:11:12 +0300

    creates new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,49 @@
 #include "dog.h"
 
 /**
+ * _strlen - finds the lenght of a string
+ * @s: the string in question
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+
+
+/**
+ * copyof - copies a given string
+ * @s: the string in question
+ * Return: string pointer
+ */
+
+char *copyof(char *s)
+{
+	int i;
+	char *cp;
+
+	cp = malloc((sizeof(char) * _strlen(s)) + 1);
+	if (cp == NULL)
+		return (NULL);
+	for (i = 0; s[i]; i++)
+	{
+		cp[i] = s[i];
+	}
+	cp[i] = '\0';
+	return (cp);
+}
+
+
+/**
  * new_dog - creates new dog
  * @name: dog's name
  * @age: dog's age
@@ -12,12 +55,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	dog->name = copyof(name);
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = copyof(owner);
 	return (dog);
 }
