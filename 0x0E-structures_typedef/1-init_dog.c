df85d3f6d25fe62cf0201c7f04be12489cb07690
MarB-T <danmarbet@gmail.com>
2023-03-20 18:43:05 +0300

    initialize struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,13 @@
  * @name: name to initialize to
  * @age: age to initialize to
  * @owner: owner to initialie to
- * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
