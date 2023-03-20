4b2a19e49c4018eb4a025d0e9ac5a84d8ab72145
MarB-T <danmarbet@gmail.com>
2023-03-20 18:34:20 +0300

    initialize struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the structure dog
+ * @d: pointer to dog
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d.name == NULL)
+		d->name = "nil";
+	if (d.owner == NULL)
+		d->owner = "nil";
+	if (d == NULL)
+		exit();
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
