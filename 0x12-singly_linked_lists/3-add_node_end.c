ae7fb24ba78f1f9c6ba7d9eac15b7b90c8ab6ec8
MarB-T <danmarbet@gmail.com>
2023-03-30 18:19:34 +0300

    Task-3: Add node to the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,14 +11,14 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *current;
-	
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		*head = new;
