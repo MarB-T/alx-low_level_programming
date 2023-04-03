d80fa20ca1a9af9a448e768b58886732c40c8f7b
MarB-T <danmarbet@gmail.com>
2023-04-03 20:14:16 +0300

    Task-4: Free a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: Head of list in question
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head->next == NULL)
+	{
+		free(head);
+		return;
+	}
+	else
+	{
+		while (head->next != NULL)
+		{
+			temp = head->next;
+			free(head);
+			head = temp;
+		}
+		free(head);
+	}
+}
