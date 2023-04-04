b8aca76a50257a0bfd238a330ed9c22c2581693a
MarB-T <danmarbet@gmail.com>
2023-04-04 15:41:39 +0300

    Task-9: Insert node at given index update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx > len)
 		return (NULL);
+	if (idx == 0)
+	{
+		return (NULL);
+	}
 	current = get_nodeint_at_index(*head, (idx - 1));
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
