5bcb6275b2e4762c2498622b021a6c08aa0c3cb1
MarB-T <danmarbet@gmail.com>
2023-04-06 17:28:17 +0300

    Task-2: Prints binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to print
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, bit, flag = 0;
+
+	for (i = 31; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	for (i = i; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		printf("%d", bit);
+	}
+	if (!flag)
+		printf("0");
+}
