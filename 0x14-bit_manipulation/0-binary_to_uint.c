07ab43ab8d03ea2ac9bf0163f5a6a9bff27e5249
MarB-T <danmarbet@gmail.com>
2023-04-06 15:58:48 +0300

    Initialization and Task-1: Binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * power - calculates exponential
+ * @base: base
+ * @power: raised
+ * Return: (base raised to power)
+ */
+
+int power(int base, int power)
+{
+	int res = 1, i;
+
+	for (i = power; i > 0; i--)
+	{
+		res = res  * base;
+	}
+	return (res);
+}
+
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: string of 1 and 0
+ * Return: converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted = 0;
+	int len = strlen(b), i, j = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (!(b[i] == '1' || b[i] == '0'))
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			converted += power(2, j);
+		}
+		j++;
+	}
+	return (converted);
+}
