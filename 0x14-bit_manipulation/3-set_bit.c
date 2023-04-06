9ad65e03cc576782858f05163a64153654720f61
MarB-T <danmarbet@gmail.com>
2023-04-06 20:59:30 +0300

    Task-3: set value of bit at given index to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of given index to 1
+ * @n: address to value to ammend
+ * @index: index to ammend
+ * Return: 1-success 1- fail
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max, j = 1;
+	unsigned long int k = 1;
+
+	while (j <= index)
+	{
+		k *= 2;
+		j++;
+	}
+	max = (sizeof(unsigned long int) * 8) - 1;
+	if (index > max)
+		return (-1);
+	else
+	{
+		*n = *n | k;
+		return (1);
+	}
+}
