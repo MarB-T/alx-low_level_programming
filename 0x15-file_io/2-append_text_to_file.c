bc10557021eb4e485490f6dc6a9ace9c3d20f3ff
MarB-T <danmarbet@gmail.com>
2023-04-11 01:03:33 +0300

    Task-2: Append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: the file in question
+ * @text_content: text to append to file
+ * Return: 1:success -1:fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	len = strlen(text_content);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
