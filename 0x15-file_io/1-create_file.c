069d09b11870d98ca7b1a9a9a62379909b51c748
MarB-T <danmarbet@gmail.com>
2023-04-10 23:52:50 +0300

    Task-1: Create file correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	len = strlen(text_content);
+	while (1)
+	{
+		if (text_content[len] != '\0')
+		{
+			len++;
+		}
+		else
+			break;
+	}
 	if (text_content == NULL)
 		len = 0;
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
