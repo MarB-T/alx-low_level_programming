2e1f347f3fcd862948dfdbfa43b2b6cd5fb82813
MarB-T <danmarbet@gmail.com>
2023-04-11 05:37:53 +0300

    Task-3: Program that copies content of a file to another file correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,42 +1,54 @@
 #include "main.h"
 
 /**
- * close_file - closes a file
- * @f: file descriptor to cose
+ * error_99 -checks error 99
+ * @buf: buffer
+ * @f: file descriptor
+ * @argv: string argument
  * Return: nothng
  */
-
-void close_file(int f)
+void error_99(int f, char *buf, char *argv)
 {
-	int closed;
-
-	closed = close(f);
-	if (closed == -1)
+	if (f < 0)
 	{
-		dprintf(STDERR_FILENO,
-				"Error: Failed to close file descriptor %d", f);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		free(buf);
+		exit(99);
 	}
 }
 
 
 /**
- * buffer - allocates space and return address
- * @file: content to write
- * Return: pointer
+ * error_100 - checks error 100
+ * @f: descriptor to check
+ * @buf: buffer
+ * Return: nothing
  */
-
-char *buffer(char *file)
+void error_100(int f, char *buf)
 {
-	char *b;
-
-	b = malloc(sizeof(char) * 1024);
-	if (b == NULL)
+	if (f < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", file);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f);
+		free(buf);
+		exit(100);
 	}
-	return (b);
+}
+
+/**
+ * error_98 - checks error 98
+ * @f: file to check
+ * @buf: buffer
+ * @argv: string argument
+ * Return: nothing
+ */
+void error_98(int f, char *buf, char *argv)
+{
+	if (f < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
+		free(buf);
+		exit(98);
+	}
 }
 
 /**
@@ -56,31 +68,25 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Syntax: copy <source_file> <destination_file>");
 		exit(97);
 	}
-	b = buffer(argv[2]);
+	b = malloc(sizeof(char) * 1024);
+	if (b == NULL)
+		return (0);
 	old = open(argv[1], O_RDONLY);
-	r = read(old, b, 1024);
+	error_98(old, b, argv[1]);
 	new = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	error_99(new, b, argv[2]);
 	do {
-		w = write(new, b, r);
-		if (old == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Encountered an error while reading from file %s\n", argv[1]);
-			free(b);
-			exit(98);
-		}
-		if (new == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Encountered an error while writing to file %s\n", argv[2]);
-			free(b);
-			exit(99);
-		}
 		r = read(old, b, 1024);
-		new = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
+		if (r == 0)
+			break;
+		error_98(r, b, argv[1]);
+		w = write(new, b, r);
+		error_99(w, b, argv[2]);
+	} while (r >= 1024);
+	r = close(new);
+	error_100(r, b);
+	w = close(old);
+	error_100(w, b);
 	free(b);
-	close_file(new);
-	close_file(old);
 	return (0);
 }
