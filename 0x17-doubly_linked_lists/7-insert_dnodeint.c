abf8863711cddb4a911a6d0e701381751bc3a8c1
MarB-T <danmarbet@gmail.com>
2023-05-19 06:01:21 +0300

    Task 7 correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	current = *h;
-	while (1)
+	while (current)
 	{
 		if (idx == count)
 		{
