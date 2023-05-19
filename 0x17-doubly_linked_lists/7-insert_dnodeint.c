03f54e5e4f235d6718211375680c0d98e66e84eb
MarB-T <danmarbet@gmail.com>
2023-05-19 05:59:47 +0300

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  * Return: address of new node
  */
 
-dlistint_t *insert_dnode_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
 	dlistint_t *new, *current, *prevv;
