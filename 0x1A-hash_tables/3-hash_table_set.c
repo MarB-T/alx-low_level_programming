5e52ed9328a1aab6ba028c255c3ba35a9a480306
MarB-T <danmarbet@gmail.com>
2023-06-09 23:41:07 +0300

    function to insert item in hash table correction4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,42 +10,42 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	char *valuecopy, *keycopy;
-	hash_node_t  *bucket, *new_node;
+	char *v, *k;
+	hash_node_t  *bucket, *new;
+	unsigned long int idx;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
-	valuecopy = strdup(value);
-	if (!valuecopy)
+	v = strdup(value);
+	k = strdup(key);
+	if (!v || !k)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	bucket = ht->array[index];
+	idx = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[idx];
 
 	while (bucket)
 	{
 		if (!strcmp(key, bucket->key))
 		{
 			free(bucket->value);
-			bucket->value = valuecopy;
+			bucket->value = v;
+			free(k);
 			return (1);
 		}
 		bucket = bucket->next;
 	}
-	new_node = calloc(1, sizeof(hash_node_t));
-	if (new_node == NULL)
+	new = calloc(1, sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(valuecopy);
+		free(v);
+		free(k);
 		return (0);
 	}
-	keycopy = strdup(key);
-	if (!keycopy)
-		return (0);
-	new_node->key = keycopy;
-	new_node->value = valuecopy;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	new->key = k;
+	new->value = v;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 	return (1);
 }
