02117ceb8cc1fe526695f0a536eef4b8e6399aaa
MarB-T <danmarbet@gmail.com>
2023-09-13 15:41:26 +0300

    Binary search correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -38,7 +38,6 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		mid = left + (right - left) / 2;
-
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
@@ -47,21 +46,14 @@ int binary_search(int *array, size_t size, int value)
 				printf(", ");
 		}
 		printf("\n");
-
 		if (array[mid] == value)
 		{
-			printf("Found %d at index %lu\n", value, mid);
 			return (mid);
 		}
 		else if (array[mid] < value)
-		{
 			left = mid + 1;
-		}
 		else
-		{
 			right = mid - 1;
-		}
 	}
-
 	return (-1);
 }
